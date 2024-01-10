e0dd7e1b4433703f9d027ea6eb221a1a9b62ca8b
Maurice <mauriceglenn2@gmail.com>
2024-01-10 11:31:17 -0600

    body mass metric mobile completed

--- components/BodyMass.tsx
+++ components/BodyMass.tsx
@@ -33,7 +33,7 @@ const BodyMass: FC<BodyMassProps> = ({}) => {
           your overall health and well-being.
         </p>
       </div>
-      <div className="border-none rounded-xl w-[328px] flex-col gap-6 bg-white">
+      <div className="border-none rounded-xl shadow-custom w-[328px] flex-col gap-6 bg-white">
         <div className="p-[24px]">
           <h1 className="font-inter text-2xl font-semibold tracking-[-1.2px] pb-[24px]">
             Enter your details below
@@ -50,21 +50,46 @@ const BodyMass: FC<BodyMassProps> = ({}) => {
               <p className="font-inter text-base font-semibold">Imperial</p>
             </div>
           </div>
-          <div className="py-[24px]">
+          <div className="pt-6">
             <p className="leading-[21px] text-sm pb-2">Height</p>
-            <input
-              className="w-full rounded-md px-6 py-5 text-2xl"
-              type="text"
-              placeholder="cm"
-            />
+            <div className="flex items-start relative">
+              <input
+                className="w-full rounded-md pl-6 pr-14 py-5 text-2xl border border-[#D8E2E7]
+                 flex items-start focus:outline-none "
+                type="text"
+              />
+              <span className="text-2xl pr-6 font-semibold text-[#345FF6] tracking-[-1.2px] items-center justify-center flex absolute transform translate-y-1/2 right-0">
+                cm
+              </span>
+            </div>
           </div>
-          <div className="py-[24px]">
+          <div className="pt-4 pb-6">
             <p className="leading-[21px] text-sm pb-2">Weight</p>
-            <input
-              className="w-full rounded-md px-6 py-5 text-2xl flex items-start"
-              type="text"
-              placeholder="kg"
-            />
+            <div className="flex items-start relative">
+              <input
+                className="w-full rounded-md pl-6 pr-14 py-5 text-2xl border border-[#D8E2E7]
+                flex items-start focus:outline-none"
+                type="text"
+              />
+              <span className="text-2xl pr-6 font-semibold text-[#345FF6] tracking-[-1.2px] items-center justify-center flex absolute transform translate-y-1/2 right-0">
+                kg
+              </span>
+            </div>
+          </div>
+          <div
+            className="p-8 flex flex-col rounded-2xl
+            bg-gradient-to-r from-[#345FF6] to-[#587DFF] w-full  border"
+          >
+            <p className="text-white font-semibold leading-6 font-inter pb-2">
+              Your BMI is...
+            </p>
+            <h1 className="font-inter text-white text-[48px] font-semibold tracking-[-2.4px ] leading-[52.8px]">
+              23.4
+            </h1>
+            <p className="pt-6 text-white font-inter text-sm font-normal leading-[21px]">
+              Your BMI suggests you’re a healthy weight. Your ideal weight is
+              between <span className="font-bold">63.3kgs - 85.2kgs.</span>
+            </p>
           </div>
         </div>
       </div>
